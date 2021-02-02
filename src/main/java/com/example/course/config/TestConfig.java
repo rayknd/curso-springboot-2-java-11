c528797d11cff00353b31dbe353c45ef6d8fd547
Ray Kennedy <ray.knd@gmail.com>
2021-02-02 15:42:07 -0300

    Entidade Product, associação many-to-many com joinTable

--- src/main/java/com/example/course/config/TestConfig.java
+++ src/main/java/com/example/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.example.course.entities.Category;
 import com.example.course.entities.Order;
+import com.example.course.entities.Product;
 import com.example.course.entities.User;
 import com.example.course.entities.enums.OrderStatus;
 import com.example.course.repositories.CategoryRepository;
 import com.example.course.repositories.OrderRepository;
+import com.example.course.repositories.ProductRepository;
 import com.example.course.repositories.UserRepository;
 
 @Configuration
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private CategoryRepository cr; 
+	
+	@Autowired
+	private ProductRepository pr;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -43,9 +48,26 @@ public class TestConfig implements CommandLineRunner{
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		
 		ur.saveAll(Arrays.asList(u1, u2));
 		or.saveAll(Arrays.asList(o1, o2, o3));
 		cr.saveAll(Arrays.asList(cat1, cat2, cat3));
+		pr.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		pr.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
 	}
 	
 	
